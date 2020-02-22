9409a370a3429f1d03dc1dda6adfa7ecda3a074b
LeiZ <zhangsanwang7@gmail.com>
2020-02-22 22:04:41 +0800

    add another print

--- CalculatorGitTest/ViewController.swift
+++ CalculatorGitTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("another hello")
         print("hello")
         // Do any additional setup after loading the view.
     }
