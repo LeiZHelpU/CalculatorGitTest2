2ab1f883fa7a503fdebe3cf8d1381f73396cd3e1
LeiZ <zhangsanwang7@gmail.com>
2020-02-26 00:47:00 +0800

    没有mvc模式的第一版本，第一节课的计算器demo

--- CalculatorGitTest/ViewController.swift
+++ CalculatorGitTest/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dispaly: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    var dispalyValue: Double {
+        get{
+            return Double(dispaly.text!)!
+        }set{
+            dispaly.text = String(newValue)
+        }
     }
+    
+    @IBAction func touchDIgit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = dispaly.text!
+            dispaly.text = textCurrentlyInDisplay + digit
+        }else{
+            dispaly.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+//        这一部分的逻辑是，当数字按下时，因为userIsInTheMiddleOfTyping是false，所以执行  dispaly.text = digit 所以display显示你按下的数字，与此同时改变userIsInTheMiddleOfTyping为true，所以当digit再次按下时会执行 let textCurrentlyInDisplay = dispaly.text! dispaly.text = textCurrentlyInDisplay + digit，所以display会显示所有你按下的数字
+      
+    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//    }
 
-
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathmaticalSymbol = sender.currentTitle{
+            switch mathmaticalSymbol {
+            case "π":
+                dispalyValue = Double.pi
+                
+            case "√":
+               dispalyValue = sqrt(dispalyValue)
+            default:
+                dispaly.text = "0"
+            }
+            
+        }
+    }
+    
 }
 
